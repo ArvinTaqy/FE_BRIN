ead4be44086e8fe1fc30ce3a9617fd4cd819cff0
ArvinTaqy <arvintaqy@mail.ugm.ac.id>
2023-04-28 00:10:47 +0700

    third

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,9 +2,14 @@ import SideBar from "../components/SideBar";
 
 function RootLayout({ children }) {
   return (
-    <div className="flex gap-5">
-      <SideBar />
-      <main className="max-w-5xl flex-1 mx-auto py-4">{children}</main>
+    <div id="main">
+      <header className="mb-3">
+        <SideBar />
+        <a href="javascript:;" className="burger-btn d-block d-xl-none">
+          <i className="bi bi-justify fs-3"></i>
+        </a>
+      </header>
+    {children}
     </div>
   );
 }
